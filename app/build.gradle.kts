0b48a67cba0d5fbf21ed8af429acb8c236400ba3
MariaLuiza-CS <m.luiza1843@gmail.com>
2026-08-23 15:01:01 -0300

    feat: adding jacoco

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     alias(libs.plugins.firebase.crashlytics)
     alias(libs.plugins.detekt)
     alias(libs.plugins.spotless)
+    id("jacoco")
 }
 
 android {
@@ -44,6 +45,9 @@ android {
                 "proguard-rules.pro",
             )
         }
+        debug {
+            enableUnitTestCoverage = true
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -59,6 +63,49 @@ android {
     testOptions {
         unitTests.isIncludeAndroidResources = true
     }
+}
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+tasks.withType<Test>().configureEach {
+    extensions.configure<JacocoTaskExtension> {
+        isIncludeNoLocationClasses = true
+        excludes = listOf("jdk.internal.*")
+    }
+}
+
+tasks.register<JacocoReport>("jacocoTestReport") {
+    dependsOn("testDebugUnitTest")
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+
+    val fileFilter = listOf(
+        "**/R.class", "**/R$*.class", "**/BuildConfig.*",
+        "**/Manifest*.*", "**/*Test*.*", "android/**/*.*",
+        "**/*_Hilt*.class", "**/Hilt_*.class", "**/*_Factory.class",
+        "**/*_MembersInjector.class", "**/*Module.class",
+        "**/dagger/hilt/**", "**/*_GeneratedInjector.class",
+        "**/*ComposableSingletons*.class", "**/ui/theme/**"
+    )
+
+    val debugTree = fileTree(
+        "${project.layout.buildDirectory.get()}/intermediates/classes/debug/transformDebugClassesWithAsm/dirs"
+    ) {
+        exclude(fileFilter)
+    }
+
+    val mainSrc = "${project.projectDir}/src/main/java"
+
+    sourceDirectories.setFrom(files(mainSrc))
+    classDirectories.setFrom(files(debugTree))
+    executionData.setFrom(fileTree(project.layout.buildDirectory.get()) {
+        include("outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec")
+    })
 }
 
 detekt {
